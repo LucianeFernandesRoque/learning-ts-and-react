92770ac0d5aba5f5fe504c5dfacea4bcc55462fc
luciane <lucianefernandescdf@gmail.com>
2022-01-19 21:59:39 -0300

    create prisma

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,10 +1,18 @@
+import axios from "axios";
+import prismaClient from "../../prisma"
 
 
 interface IAccessTokenResponse {
-  access_token: string
+  access_token: string;
 }
 
-import axios from "axios";
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string 
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "http://github.com/login/oauth/access_token";
@@ -18,14 +26,37 @@ class AuthenticateUserService {
       headers: {
         Accept: "application/json",
       },
-    });
+    }
+  );
 
-    const response = await axios.get("https://api.github.com/user", {
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
       headers: {
       authorization: `Bearer ${accessTokenResponse.access_token}`,
       },
-    });
-      return response.data;
+    }
+  );
+
+    const { login, id, avatar_url, name } = response.data
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+
+    if(!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+
+    }
+
+    return response.data;
   }
 }
 
